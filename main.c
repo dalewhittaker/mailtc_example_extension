9d7194b24e300cf20db7cd9ae3386e59a6972e4b
Dale Whittaker <dalewhittaker@users.noreply.github.com>
2022-08-02 19:26:19 +0100

    Update email and copyright

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /* main.c
- * Copyright (C) 2009-2012 Dale Whittaker <dayul@users.sf.net>
+ * Copyright (C) 2009-2022 Dale Whittaker
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
